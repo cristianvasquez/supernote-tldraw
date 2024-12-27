e8b6f1f48f46f1d01e28534fbbe04c4f95f8f439
Cristian <cristianvasquez@gmail.com>
2024-12-27 20:39:15 +0100

    handle files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
-import { Tldraw } from 'tldraw'
+import { createShapesForAssets, Tldraw, Vec } from 'tldraw'
 import 'tldraw/tldraw.css'
 
-export default function App() {
+export default function App () {
   const handleMount = (editor) => {
-    editor.createShape({
-      type: 'text',
-      x: 200,
-      y: 200,
-      props: {
-        text: 'Hello world!',
-      },
-    })
+    editor.registerExternalContentHandler('files', async ({ point, files }) => {
+      if (files.length > editor.options.maxFilesAtOnce) {
+        throw new Error('Too many files')
+      }
 
-    editor.selectAll()
+      const position =
+        point ??
+        (editor.inputs.shiftKey
+          ? editor.inputs.currentPagePoint
+          : editor.getViewportPageBounds().center)
 
-    editor.zoomToSelection({
-      animation: { duration: 5000 },
+      for (const file of files) {
+        console.log('file', file)
+        editor.createShape({
+          type: 'geo',
+          x: position.x,
+          y: position.y,
+          props: {
+            w: 200,
+            h: 100,
+            text: file.name,
+            fill: 'solid',
+            geo: 'rectangle',
+          },
+        })
+
+      }
     })
   }
 
   return (
     <div style={{ position: 'fixed', inset: 0 }}>
-      <Tldraw onMount={handleMount} />
+      <Tldraw onMount={handleMount}/>
     </div>
   )
 }
